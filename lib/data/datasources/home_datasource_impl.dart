e4123a0d9b068f05545276ef84f9ef6c633e515a
Johnathan Silva <johnathans@apexglobal.co>
2025-07-22 01:07:32 -0500

    add datasources, repositories, usecases

---
+++ lib/data/datasources/home_datasource_impl.dart
@@ -0,0 +1,27 @@
+import 'package:planets_3A/core/utils/custom_console.dart';
+import 'package:planets_3A/data/api/api_client.dart';
+import 'package:planets_3A/data/models/base/base_response.dart';
+import 'package:planets_3A/data/models/planet_model.dart';
+import 'package:planets_3A/domain/datasources/home_datasource.dart';
+import 'package:planets_3A/domain/enum/log_level_enum.dart';
+
+class HomeDatasourceImpl implements HomeDatasource {
+  late final ApiClient _apiClient;
+
+  HomeDatasourceImpl() {
+    _apiClient = ApiClient();
+  }
+
+  @override
+  Future<List<PlanetModel>> getPlanets() async {
+    try {
+      final BaseResponse response = await _apiClient.getPlanets();
+      final List<PlanetModel> planets =
+          (response.data as List<dynamic>).map((element) => PlanetModel.fromJson(element)).toList();
+      return planets; // Ya es List<PlanetModel>
+    } catch (e) {
+      CustomConsole.log(message: 'Error to get planets', error: e.toString(), level: LogLevel.warning);
+      rethrow;
+    }
+  }
+}
