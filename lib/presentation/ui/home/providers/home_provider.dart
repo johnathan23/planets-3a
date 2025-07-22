afce843628269a49999d1447da222211f9dcacb0
Johnathan Silva <johnathans@apexglobal.co>
2025-07-22 01:07:45 -0500

    add home provider

--- lib/presentation/ui/home/providers/home_provider.dart
+++ lib/presentation/ui/home/providers/home_provider.dart
@@ -1,1 +1,35 @@
+import 'package:planets_3A/data/datasources/home_datasource_impl.dart';
+import 'package:planets_3A/data/models/planet_model.dart';
+import 'package:planets_3A/data/repositories/home_repository_impl.dart';
+import 'package:planets_3A/domain/datasources/home_datasource.dart';
+import 'package:planets_3A/domain/repositories/home_repository.dart';
+import 'package:planets_3A/domain/usecases/home_usecase.dart';
+import 'package:planets_3A/presentation/ui/home/states/home_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'home_provider.g.dart';
+
+@riverpod
+class HomeProvider extends _$HomeProvider {
+  @override
+  HomeState build() {
+    return HomeState();
+  }
+
+  HomeUseCase get _homeUseCase {
+    final HomeRepository homeRepository = HomeRepositoryImpl();
+    return HomeUseCase(homeRepository);
+  }
+
+  Future<void> getPlanets() async {
+    state = state.copyWith(isLoading: true, hasError: false);
+    try {
+      final List<PlanetModel> planets = await _homeUseCase.execute();
+      state = state.copyWith(planets: planets, isLoading: false);
+    } catch (e) {
+      state = state.copyWith(isLoading: false, hasError: true, errorMessage: e.toString());
+
+      throw Exception('Error get planets: $e');
+    }
+  }
+}
--- lib/presentation/ui/home/states/home_state.dart
+++ lib/presentation/ui/home/states/home_state.dart
@@ -1,1 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:planets_3A/data/models/planet_model.dart';
 
+part 'home_state.freezed.dart';
+part 'home_state.g.dart';
+
+@freezed
+abstract class HomeState with _$HomeState {
+  const factory HomeState({
+    List<PlanetModel>? planets,
+    @Default(false) bool isLoading,
+    @Default(false) bool hasError,
+    String? errorMessage,
+  }) = _HomeState;
+
+  factory HomeState.fromJson(Map<String, dynamic> json) => _$HomeStateFromJson(json);
+}
